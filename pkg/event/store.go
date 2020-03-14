1611958d5b675ab1aba2e2e49d94b0196c15aca0
Rafał Korkosz <korkosz.rafal@gmail.com>
2020-03-14 20:45:01 +0100

    Add Get method to Store
    Signed-off-by: Rafał Korkosz <korkosz.rafal@gmail.com>

--- pkg/event/store.go
+++ pkg/event/store.go
@@ -2,33 +2,45 @@ package event
 
 import (
 	"encoding/json"
+	"fmt"
 	"sync"
 
+	"github.com/google/uuid"
 	bolt "go.etcd.io/bbolt"
 )
 
 // Store describes event store methods
 type Store interface {
 	Put(Event) error
+	Get(ID uuid.UUID) (*Event, error)
 }
 
 // InMemoryStore stores events in memory
 type InMemoryStore struct {
 	sync.RWMutex
-	db map[string]Event
+	db map[string]*Event
 }
 
 // NewInMemoryStore creates InMemoryStore
 func NewInMemoryStore() *InMemoryStore {
-	return &InMemoryStore{db: make(map[string]Event)}
+	return &InMemoryStore{db: make(map[string]*Event)}
 }
 
 // Put implements Store.Put method for InMemoryStore
-func (ms *InMemoryStore) Put(e Event) error {
+func (ms *InMemoryStore) Put(e *Event) error {
 	ms.Lock()
 	ms.db[e.ID.String()] = e
 	ms.Unlock()
 	return nil
+}
+
+// Get retrieves Event by id
+func (ms *InMemoryStore) Get(id uuid.UUID) (*Event, error) {
+	event, ok := ms.db[id.String()]
+	if !ok {
+		return nil, fmt.Errorf("There is no event with ID: %s", id)
+	}
+	return event, nil
 }
 
 // BoltStore implements Store interface with bbolt database
@@ -52,7 +64,7 @@ func NewBoltStore(dbPath string, dbOptions *bolt.Options) (*BoltStore, error) {
 }
 
 // Put implements Store.Put method
-func (bs *BoltStore) Put(e Event) error {
+func (bs *BoltStore) Put(e *Event) error {
 	db, err := bolt.Open(bs.dbPath, 0600, bs.dbOptions)
 	if err != nil {
 		return err
@@ -60,10 +72,26 @@ func (bs *BoltStore) Put(e Event) error {
 	defer db.Close()
 	return db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte("events"))
-		data, err := json.Marshal(&e)
+		data, err := json.Marshal(e)
 		if err != nil {
 			return err
 		}
 		return b.Put([]byte(e.ID.String()), data)
 	})
+}
+
+// Get gets event from store
+func (bs *BoltStore) Get(id uuid.UUID) (*Event, error) {
+	event := &Event{}
+	db, err := bolt.Open(bs.dbPath, 0600, bs.dbOptions)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	err = db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("events"))
+		data := b.Get([]byte(id.String()))
+		return json.Unmarshal(data, event)
+	})
+	return event, nil
 }
---
+++ pkg/event/store_test.go
@@ -0,0 +1,26 @@
+package event
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+)
+
+func TestInMemoryStore_(t *testing.T) {
+	store := NewInMemoryStore()
+	event := &Event{
+		ID:          uuid.New(),
+		SpecVersion: "v1",
+	}
+	err := store.Put(event)
+	if err != nil {
+		t.Error(err)
+	}
+	storedEvent, err := store.Get(event.ID)
+	if err != nil {
+		t.Error(err)
+	}
+	if event != storedEvent {
+		t.Errorf("Wrong event version: got %s, want %s", storedEvent.SpecVersion.event.SpecVersion)
+	}
+}
