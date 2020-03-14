7b7a051a3468e0bb5d30987a1858cd4c4755ef09
Rafał Korkosz <korkosz.rafal@gmail.com>
2020-03-14 19:45:13 +0100

    Add bolt store
    Signed-off-by: Rafał Korkosz <korkosz.rafal@gmail.com>

--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.13
 require (
 	github.com/go-chi/chi v4.0.3+incompatible
 	github.com/google/uuid v1.1.1
+	go.etcd.io/bbolt v1.3.3
 	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
 )
--- pkg/event/store.go
+++ pkg/event/store.go
@@ -1,6 +1,11 @@
 package event
 
-import "sync"
+import (
+	"encoding/json"
+	"sync"
+
+	bolt "go.etcd.io/bbolt"
+)
 
 // Store describes event store methods
 type Store interface {
@@ -24,4 +29,41 @@ func (ms *InMemoryStore) Put(e Event) error {
 	ms.db[e.ID.String()] = e
 	ms.Unlock()
 	return nil
+}
+
+// BoltStore implements Store interface with bbolt database
+type BoltStore struct {
+	dbPath    string
+	dbOptions *bolt.Options
+}
+
+// NewBoltStore creates BoltStore object
+func NewBoltStore(dbPath string, dbOptions *bolt.Options) (*BoltStore, error) {
+	db, err := bolt.Open(dbPath, 0600, dbOptions)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("events"))
+		return err
+	})
+	return &BoltStore{dbPath: dbPath}, err
+}
+
+// Put implements Store.Put method
+func (bs *BoltStore) Put(e Event) error {
+	db, err := bolt.Open(bs.dbPath, 0600, bs.dbOptions)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	return db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("events"))
+		data, err := json.Marshal(&e)
+		if err != nil {
+			return err
+		}
+		return b.Put([]byte(e.ID.String()), data)
+	})
 }
